eac17e336b66d9fa3fbd907570395454c007cf55
rkristof96 <reizingerkristof96@gmail.com>
2022-01-18 20:03:53 +0100

    working wordle python code

--- wordle.py
+++ wordle.py
@@ -5,17 +5,20 @@ def score_letter(letter, position, true_word):
     if(true_word[position] == letter):
         return '*' # for correct position
 
-        if(letter in true_word):
-            return 'o' # for correct letter in incorrectr position
-        else:
-            return '-'    
+
+    if(letter in true_word):
+           return 'o' # for correct letter in incorrectr position
+    else:
+           return '-'    
 
 def score_word(guess, true_word):
     # score_word('APPL','ABBA')
     # results '*---'
-
+    # algorithm works for words with the same length
     N=len(true_word)
     for position in range(N):
         print(position, guess[position],score_letter(guess[position], position,true_word))
 
 score_word('AAPL', 'ABBA')
+
+# FIXME: add functions to get user inputs & read random word
