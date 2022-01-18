f2beb732639fdf30cf2687f68b61dd098b900b05
rkristof96 <reizingerkristof96@gmail.com>
2022-01-18 17:22:18 +0100

    message wordle.py

---
+++ wordle.py
@@ -0,0 +1,21 @@
+def score_letter(letter, position, true_word):
+
+    # for example score_letter ('A',1, 'ABBA') should be a correct position
+
+    if(true_word[position] == letter):
+        return '*' # for correct position
+
+        if(letter in true_word):
+            return 'o' # for correct letter in incorrectr position
+        else:
+            return '-'    
+
+def score_word(guess, true_word):
+    # score_word('APPL','ABBA')
+    # results '*---'
+
+    N=len(true_word)
+    for position in range(N):
+        print(position, guess[position],score_letter(guess[position], position,true_word))
+
+score_word('AAPL', 'ABBA')
