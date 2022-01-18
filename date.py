6c5d5c7030da629d037d1eb7d7c053e44aeadbbd
rkristof96 <reizingerkristof96@gmail.com>
2022-01-18 16:57:12 +0100

    functions

--- date.py
+++ date.py
@@ -1,7 +1,17 @@
 date = '2022-01-17'
 
-year=int(date[0:4])
-month=int(date[5:7])
-day=int(date[-2:])
+def compute_tomorrow(date):
+    year=date[0:4]
+    month=date[5:7]
+    day=int(date[-2:])
+    return year + '-' + month + '-' + str(day + 1)
 
-print('Next class is on ',year, month, day + 1)
+
+print('Next class is on ',compute_tomorrow(date))
+
+
+def feet2meter(feet):
+     meter=0.3048*feet  
+     return(meter)
+
+print('200 feet in meters =', feet2meter(200))
