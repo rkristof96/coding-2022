921f15d84e3b998af8f166b62107ce470bd8d56d
rkristof96 <reizingerkristof96@gmail.com>
2022-01-19 16:21:32 +0100

    nested functions

--- practice.py
+++ practice.py
@@ -6,6 +6,13 @@ def convert_inches_to_feet(distance_inches):
     return distance_inches /12.0
 
 
+def convert_inches_to_meters(distance_inches):
+    # distance_feet=convert_inches_to_feet(distance_inches)
+    # distance_meter = convert_feet_to_meters(distance_feet)
+    distance_meter= convert_feet_to_meters(convert_inches_to_feet(distance_inches))
+    return distance_meter
+
+
 print('I am here.')
 distance_meter=10.0
 print(distance_meter)
@@ -15,3 +22,4 @@ print('Now I am done.')
 
 
 print('1000 inches in meters are', convert_feet_to_meters(convert_inches_to_feet(1000.0)))
+print('1000 inches in meters are', convert_inches_to_meters(1000.0))
